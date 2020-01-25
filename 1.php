1985ed319ab490101a08a59b0d71dd74a18c126d
DaffaAM <45245291+DaffaAM@users.noreply.github.com>
2020-01-25 20:15:46 +0700

    initial commit

---
+++ 1.php
@@ -0,0 +1,16 @@
+<?php
+$lovebird = 6969;
+$tewas = 441/21;
+$simpan;
+$akhir;
+
+for ($i=0; $i<$tewas; $i++) {
+
+$simpan = $lovebird * (11.1/100) ;
+$hasil = $lovebird - $simpan * 21;
+$akhir = $hasil + $lovebird;
+
+}
+
+echo "Jumlah Lovebird Selama 441 Hari adalah $akhir";
+ ?>
---
+++ 2.php
@@ -0,0 +1,18 @@
+<?php
+
+//i hope this is right :'(
+
+$dataLain=null;
+function minmax($dataLain)
+{
+  $b =  min('a','b','c','d');
+  $a =  max('a','b','c','d');
+
+
+$var = array ($b,$a);
+print_r($var);
+
+}
+
+
+minmax($dataLain);
