3a059d4fe08567679b33f905bd82891d54a19a47
DaffaAM <45245291+DaffaAM@users.noreply.github.com>
2020-01-25 20:29:16 +0700

    initial commit

---
+++ dumblibrary.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 25, 2020 at 02:28 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.1.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dumblibrary`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_tb`
+--
+
+CREATE TABLE `book_tb` (
+  `id` int(11) NOT NULL,
+  `name` varchar(300) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `writer_id` int(11) NOT NULL,
+  `Publication_year` int(11) NOT NULL,
+  `img` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `book_tb`
+--
+
+INSERT INTO `book_tb` (`id`, `name`, `category_id`, `writer_id`, `Publication_year`, `img`) VALUES
+(1, 'Pemrograman Sistem Pakar', 1, 1, 2017, '1.jpg'),
+(2, 'Belajar Pemrograman Java Untuk Pemula', 2, 2, 2014, '2.png'),
+(3, 'Pemrograman Javascript Untuk Aplikasi WEB', 3, 3, 2019, '3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category_tb`
+--
+
+CREATE TABLE `category_tb` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category_tb`
+--
+
+INSERT INTO `category_tb` (`id`, `name`) VALUES
+(1, 'Visual Basic'),
+(2, 'Java'),
+(3, 'Javascript');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `writername_tb`
+--
+
+CREATE TABLE `writername_tb` (
+  `id` int(11) NOT NULL,
+  `writer` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `writername_tb`
+--
+
+INSERT INTO `writername_tb` (`id`, `writer`) VALUES
+(1, 'Didik Setiawan'),
+(2, 'Anik Andriani, M. Kom.'),
+(3, 'Abdul Kadir'),
+(4, 'Betha Sidik');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book_tb`
+--
+ALTER TABLE `book_tb`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `category_tb`
+--
+ALTER TABLE `category_tb`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `writername_tb`
+--
+ALTER TABLE `writername_tb`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book_tb`
+--
+ALTER TABLE `book_tb`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `category_tb`
+--
+ALTER TABLE `category_tb`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `writername_tb`
+--
+ALTER TABLE `writername_tb`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
